7341f09c82f4c9ac06cc8ce58eaab8ab19119e36
phonerepairdoc-uk <paul@phonerepairdoctor.co.uk>
2025-01-30 15:39:39 +0000

    Update repair_workflow.py

--- repair_workflow.py
+++ repair_workflow.py
@@ -1,5 +1,3 @@
-Python 3.13.1 (v3.13.1:06714517797, Dec  3 2024, 14:00:22) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
 import streamlit as st
 import pandas as pd
 import smtplib
